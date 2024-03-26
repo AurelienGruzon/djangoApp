dc4badc076d56ff2930bd5d83a1ebc5c0f09edfd
AurelienGruzon <aureliengruzon@gmail.com>
2024-03-26 16:14:16 +0100

    update

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,9 +3,15 @@ from django.shortcuts import render
 from listings.models import Band
 from listings.models import Listing
 
-def hello(request):
+
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, "listings/hello.html", context = {"bands": bands})
+    return render(request, "listings/band_list.html", context = {"bands": bands})
+
+def band_detail(request, id):
+
+    band = Band.objects.get(id = id)
+    return render(request, "listings/band_detail.html", {'band': band})
 
 def about(request):
     return render(request, "listings/about.html")
