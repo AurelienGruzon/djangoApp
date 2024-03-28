8888838844aadb8f66e1a9fde9ebc0f4f87db08f
AurelienGruzon <aureliengruzon@gmail.com>
2024-03-28 12:17:01 +0100

    update

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,7 +5,11 @@ from listings.models import Listing
 from listings.forms import ContactUsForm
 from django.core.mail import send_mail
 from django.shortcuts import redirect
+from listings.forms import BandForm
+from listings.forms import ListForm
+
 def band_list(request):
+
     bands = Band.objects.all()
     return render(request, "listings/band_list.html", context = {"bands": bands})
 
@@ -15,13 +19,16 @@ def band_detail(request, id):
     return render(request, "listings/band_detail.html", {'band': band})
 
 def about(request):
+
     return render(request, "listings/about.html")
 
 def listings(request):
+
     listings = Listing.objects.all()
     return render(request, "listings/listings.html", context = {"listings": listings})
 
 def listing_detail(request, id):
+
     listing = Listing.objects.get(id = id)
     return render(request, "listings/listing_detail.html", {'listing' : listing})
 
@@ -43,11 +50,43 @@ def contact_us(request):
     # ci-dessous et afficher à nouveau le formulaire (avec des erreurs).
     else:
         form = ContactUsForm()
-    return render(
-        request,
-        "listings/contact.html",
-        {'form' : form}
-    )
+    return render(request, "listings/contact.html", {'form' : form})
 
 def email_sent(request):
+
     return render(request, 'listings/email_sent.html')
+
+def band_create(request):
+
+    if request.method == 'POST':
+        form = BandForm(request.POST)
+        if form.is_valid():
+            # créer une nouvelle « Band » et la sauvegarder dans la db
+            band = form.save()
+            # redirige vers la page de détail du groupe que nous venons de créer
+            # nous pouvons fournir les arguments du motif url comme arguments à la fonction de redirection
+            return redirect('band-detail', band.id)
+
+    else:
+        form = BandForm()
+
+    return render(request,
+            'listings/band_create.html',
+            {'form': form})
+
+
+def listing_create(request):
+
+    if request.method == 'POST':
+        form = ListForm(request.POST)
+        if form.is_valid():
+            listing = form.save()
+            return redirect('listing-detail', listing.id)
+    else:
+        form = ListForm()
+
+        return render(request, 'listings/listing_create.html', {'form': form})
+
+def band_change(request, id):
+    band = Band.objects.get(id = id)
+    return render(request, 'listings/band_change.html', {'band' : band}))
--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -23,9 +23,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('bands/', views.band_list, name = 'band-list'),
     path('bands/<int:id>/', views.band_detail, name = 'band-detail'),
+    path('bands/add/', views.band_create, name = 'band-create'),
     path('about-us/', views.about, name = 'about'),
     path('listings/', views.listings, name = "listings"),
     path('listings/<int:id>/', views.listing_detail, name = 'listing-detail'),
+    path('listings/add/', views.listing_create, name = 'listing-create'),
     path('contact-us/', views.contact_us, name = 'contact-us'),
     path('email-sent/', views.email_sent, name = 'email-sent'),
 ]
